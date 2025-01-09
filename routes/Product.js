0339fc6915c8bb0acadb98a892030230316e40ba
reyazul007 <reyazulansari296@gmail.com>
2025-01-09 17:18:41 +0545

    backend product

--- routes/Product.js
+++ routes/Product.js
@@ -41,5 +41,55 @@ router.post(
     }
   }
 );
+router.put("/updateproduct/:id", fecthUser, async (req, res) => {
+  const { title, description, price, instock } = req.body;
+  try {
+    const newProduct = {};
+    if (title) {
+      newProduct.title = title;
+    }
+    if (description) {
+      newProduct.description = description;
+    }
+    if (price) {
+      newProduct.price = price;
+    }
+    if (instock) {
+      newProduct.instock = instock;
+    }
+
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    if (!product.user || product.user.toString() !== req.user.id) {
+      return res.status(404).send("not allowed");
+    }
+    product = await Product.findByIdAndUpdate(
+      req.params.id,
+      { $set: newProduct },
+      { new: true }
+    );
+    res.json(product);
+  } catch (error) {
+    res.status(500).send("internal server error", error.message);
+  }
+});
+
+router.delete("/deleteproduct/:id", fecthUser, async (req, res) => {
+  try {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).send("product not found");
+    }
+    if (product.user.toString() !== req.user.id) {
+      return res.status(404).send("not allowed");
+    }
+    product = await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: "product deleted" });
+  } catch (error) {
+    res.status(500).send("internal server error", error);
+  }
+});
 
 module.exports = router;
